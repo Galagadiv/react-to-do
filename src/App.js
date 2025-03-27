cc350ab84d895ab3b040d59bf6f785a135dd2e35
Galagadiv <sobolia37@gmail.com>
2025-03-27 22:30:27 +0200

    exhange rate works

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+
+import "./App.css";
 
 function App() {
+	const [rate, setRate] = useState([]);
+
+	useEffect(() => {
+		fetch("https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				setRate(data);
+			});
+	}, []);
+
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
+		<div>
+			<header>
+				<h1>Курс валют</h1>
+				<div style={{height: "100%"}}>
+					<button>Reload</button>
+				</div>
 			</header>
+			<main>
+				<ul>
+					<li style={{backgroundColor: "#9198a3"}}>
+						<div style={{display: "flex", textAlign: "center", gap: 5}}>
+							<h3 style={{width: 100}}>$</h3>
+							<h3>Назва валюти</h3>
+						</div>
+						<h3>Курс до грн.</h3>
+					</li>
+
+					{rate.map((el) => (
+						<li>
+							<div style={{display: "flex", textAlign: "center", gap: 5}}>
+								<h3 style={{width: 100}}>{el.cc}</h3>
+								<h3>{el.txt}</h3>
+							</div>
+
+							<h3>{el.rate}</h3>
+						</li>
+					))}
+				</ul>
+			</main>
 		</div>
 	);
 }
